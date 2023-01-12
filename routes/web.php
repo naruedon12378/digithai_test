15a544424bf6ce117186fa8bf909bdb514b09251
naruedon12378 <naruedon12378@gmail.com>
2023-01-12 14:09:48 +0700

    Initial project with Laravel 8,AdminLTE,Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+//disable register,reset password and email verify feature
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false,
+]);
+
