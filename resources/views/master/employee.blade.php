22a8bd09ecf2d66107f2d33ce08254c777d17be9
naruedon12378 <naruedon12378@gmail.com>
2023-01-13 13:43:30 +0700

    edit datatable responsive

--- resources/views/master/employee.blade.php
+++ resources/views/master/employee.blade.php
@@ -107,8 +107,13 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.min.js" integrity="sha512-rstIgDs0xPgmG6RX1Aba4KV5cWJbAMcvRCVmglpam9SoHZiUCyQVDdH2LPlxoHtrv17XWblE/V/PP+Tr04hbtA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
-    <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
+    <link href="https://cdn.datatables.net/responsive/2.2.4/css/responsive.dataTables.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.datatables.net/responsive/2.2.4/js/dataTables.responsive.js"></script>
+    <link href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.dataTables.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.colVis.js"></script>
 
     <script>
         $(function() {
@@ -149,7 +154,7 @@
                         },className: 'text-center'},
                 ],
                 columnDefs: [
-                    { responsivePriority: 1, targets: [0,1,5] },
+                    { responsivePriority: 1, targets: [0,4,5,1] },
                 ],
                 "dom": '<"top my-1 mr-1"f>rt<"bottom d-flex position-absolute w-100 justify-content-between px-1 mt-3"ip><"clear">'
             });
@@ -173,7 +178,6 @@
                             $('#email').val(response.email);
                             $('#phone').val(response.phone);
                             $('#company').val(response.company).change();
-
                         }
                     });
                 }
